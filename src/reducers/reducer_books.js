527885e831a40fde09c36dee2cda9e02d67b6713
Crystal Jones <cjones@starmark.com>
2018-02-07 16:45:31 -0500

    containers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function () {
+	return [
+		{title: 'Doctor Sleep'},
+		{title: 'The Shining'},
+		{title: 'Salems Lot'},
+		{title: 'The Stand'}
+	]
+}
+
+// any other file can now import this file and receive books reducer 
